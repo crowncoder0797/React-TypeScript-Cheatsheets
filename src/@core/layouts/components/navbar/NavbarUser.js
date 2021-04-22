249bccd8c124d8b32de09257bb4426624d287774
fullstackdeveloper1206 <bluestar90116@gmail.com>
2021-04-23 04:48:22 +0800

    clean product

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,9 +1,7 @@
 // ** Dropdowns Imports
 import IntlDropdown from './IntlDropdown'
-import CartDropdown from './CartDropdown'
 import UserDropdown from './UserDropdown'
 import NavbarSearch from './NavbarSearch'
-import NotificationDropdown from './NotificationDropdown'
 
 // ** Third Party Components
 import { Sun, Moon } from 'react-feather'
@@ -31,8 +29,6 @@ const NavbarUser = props => {
         </NavLink>
       </NavItem>
       <NavbarSearch />
-      <CartDropdown />
-      <NotificationDropdown />
       <UserDropdown />
     </ul>
   )
--- src/router/routes/Dashboards.js
+++ src/router/routes/Dashboards.js
@@ -3,10 +3,6 @@ import { lazy } from 'react'
 const DashboardRoutes = [
   // Dashboards
   {
-    path: '/dashboard/analytics',
-    component: lazy(() => import('../../views/dashboard/analytics'))
-  },
-  {
     path: '/dashboard/ecommerce',
     component: lazy(() => import('../../views/dashboard/ecommerce')),
     exact: true
