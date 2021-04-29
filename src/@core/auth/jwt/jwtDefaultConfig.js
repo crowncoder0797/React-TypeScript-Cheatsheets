7292aca881faa35586389933d7e50128f1ebad41
fullstackdeveloper1206 <bluestar90116@gmail.com>
2021-04-29 12:41:42 +0800

    tasks manager update

--- src/@core/auth/jwt/jwtDefaultConfig.js
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,6 +1,5 @@
 // ** Auth Endpoints
-const url = 'http://h2931731.stratoserver.net'
-const test = 'http://localhost:20127'
+const url = process.env.NODE_ENV === 'production' ? 'https://vr.myyaak.com' : 'http://localhost:20127'
 export default {
   loginEndpoint: 'user/login',
   refreshEndpoint: 'users/refresh-token',
@@ -13,7 +12,7 @@ export default {
   storageTokenKeyName: 'accessToken',
   storageRefreshTokenKeyName: 'refreshToken',
   Socket: null,
-  apiUrl: `${test}/admin/`,
-  fileUrl: `${test}/upload/`,
-  Domain: `${test}`
+  apiUrl: `${url}/admin/`,
+  fileUrl: `${url}/upload/`,
+  Domain: `${url}`
 }
--- src/views/tasks/index.js
+++ src/views/tasks/index.js
@@ -3,8 +3,10 @@ import { Button, CustomInput, Form, FormGroup, Input, Label, Row } from "reactst
 import DataTable from "react-data-table-component"
 import { ChevronDown, Trash2 } from "react-feather"
 import ReactPaginate from "react-paginate"
-import MapGL, {Marker, NavigationControl} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+// import MapGL, {Marker, NavigationControl} from 'react-map-gl'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp'
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import useJwt from '@src/auth/jwt/useJwt'
 import MarkerLogo from '../../assets/images/pages/marker.png'
 import Col from "reactstrap/lib/Col"
@@ -12,26 +14,26 @@ import Toasts from '../Toasts'
 import { Slide, toast } from "react-toastify"
 import jwtDefaultConfig from '@src/@core/auth/jwt/jwtDefaultConfig'
 
-const accessToken = "pk.eyJ1IjoibWFsZXRpZ2VyIiwiYSI6ImNrbjQ5eGMxcTA0dDIydm9mbjY4Zm8wOHMifQ.TWABwlDJkdgUACO1DMeBPw"
+mapboxgl.workerClass = MapboxWorker
+mapboxgl.accessToken = "pk.eyJ1IjoibWFsZXRpZ2VyIiwiYSI6ImNrbjQ5eGMxcTA0dDIydm9mbjY4Zm8wOHMifQ.TWABwlDJkdgUACO1DMeBPw"
+const markerStyle = `width: 42px; height: 58px; background-size: contain; background-repeat: no-repeat; background-image: url(${MarkerLogo})`
 
 const Tasks = () => {
+  const mapContainer = useRef()
   const [showAllTasks, setShowAllTasks] = useState(false)
   const [data, setData] = useState([])
   const [taskName, setTaskName] = useState("")
   const [taskFile, setTaskFile] = useState(undefined)
   const [perPage] = useState(10)
   const [currentPage, setCurrentPage] = useState(0)
-  const [viewport, setViewport] = useState({
-    longitude: 151.21490299701586,
-    latitude: -33.857362785537184,
-    zoom: 16,
-    bearing: 0,
-    pitch: 0
-  })
-  const [marker, setMarker] = useState({
-    longitude: 151.21490299701586,
-    latitude: -33.857362785537184
-  })
+  const [center, setCenter] = useState([
+    151.21490299701586,
+    -33.857362785537184
+  ])
+  const [marker, setMarker] = useState([
+    151.21490299701586,
+    -33.857362785537184
+  ])
 
   const deleteTask = (_id) => {
     useJwt.deleteTask({_id}).then(({data})  => {
@@ -41,7 +43,7 @@ const Tasks = () => {
       } else {
         toast.error(Toasts.ErrorToast(data.data), { transition: Slide, hideProgressBar: true, autoClose: 2000 })
       }
-    }).catch((err) => { console.log(`err`, err) });
+    }).catch((err) => { console.log(`err`, err) })
   }
 
   const [columns] = useState([
@@ -101,9 +103,9 @@ const Tasks = () => {
   }
 
   const addTask = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const taskData = new FormData()
-    taskData.append("position", JSON.stringify({lat: marker.latitude, lng: marker.longitude}))
+    taskData.append("position", JSON.stringify({lat: marker[1], lng: marker[0]}))
     taskData.append("taskName", taskName)
     taskData.append("taskFile", taskFile)
     
@@ -114,7 +116,37 @@ const Tasks = () => {
       } else {
         toast.error(Toasts.ErrorToast(data.data), { transition: Slide, hideProgressBar: true, autoClose: 2000 })
       }
-    }).catch((err) => { console.log(`err`, err) });
+    }).catch((err) => { console.log(`err`, err) })
+  }
+
+  const onMarkerDragEnd = (e) => {
+    setMarker([
+      e.target._lngLat.lng,
+      e.target._lngLat.lat
+    ])
+  }
+
+  const renderAllTasks = (Map) => {
+    
+    data.forEach((item, i) => {
+      const markerDiv = document.createElement('div')
+      markerDiv.style.cssText = markerStyle
+      
+      new mapboxgl.Marker(markerDiv).setLngLat([item.task_position.lng, item.task_position.lat])
+      .addTo(Map)
+    })
+  }
+
+  const renderAMarker = (Map) => {
+    const markerDiv = document.createElement('div')
+    markerDiv.style.cssText = markerStyle
+
+    const Marker = new mapboxgl.Marker(markerDiv, {
+      draggable: true
+    }).setLngLat(marker)
+      .addTo(Map)
+
+    Marker.on('dragend', onMarkerDragEnd)
   }
   
   useEffect(() => {
@@ -122,26 +154,29 @@ const Tasks = () => {
       if (data.status === true) {
         setData(data.data)
         if (data.data.length > 0) {
-          setViewport({
-            ...viewport,
-            latitude: data.data[0].task_position.lat,
-            longitude: data.data[0].task_position.lng
-          })
-          setMarker({
-            latitude: data.data[0].task_position.lat,
-            longitude: data.data[0].task_position.lng
-          })
+          setCenter([
+            data.data[0].task_position.lng,
+            data.data[0].task_position.lat
+          ])
+          setMarker([
+            data.data[0].task_position.lng,
+            data.data[0].task_position.lat
+          ])
         }
       }
-    }).catch((err) => { console.log(`err`, err) });
+    }).catch((err) => { console.log(`err`, err) })
   }, [])
-
-  const onMarkerDragEnd = (e) => {
-    setMarker({
-      longitude: e.lngLat[0],
-      latitude: e.lngLat[1]
+  
+  useEffect(() => {
+    const Map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center,
+      zoom: 17.2,
+      container: mapContainer.current
     })
-  }
+  
+    showAllTasks ? renderAllTasks(Map) : renderAMarker(Map)
+  }, [showAllTasks])
 
   const CustomPagination = () => (
     <ReactPaginate
@@ -170,7 +205,7 @@ const Tasks = () => {
     <div className='misc-wrapper'>
       <div className="w-100 mb-1 position-relative" style={{height: "400px", overflow: "hidden", borderRadius: 10}}>
         <Button color="primary" className="position-absolute" style={{zIndex: 999, right: 10, top: 10}} onClick={() => handleShowAllTasks()}>{showAllTasks ? `Hide` : `Show all tasks`}</Button>
-        <MapGL
+        {/* <MapGL
           {...viewport}
           width="100%"
           height="100%"
@@ -196,40 +231,41 @@ const Tasks = () => {
                 <div style={{backgroundImage: `url(${MarkerLogo})`, width: 44, height: 58, backgroundRepeat:"no-repeat", backgroundSize: "contain"}}></div>
               </Marker>
           }
-        </MapGL>
+        </MapGL> */}
+        <div className="w-100 h-100" ref={mapContainer} />
       </div>
       {
         showAllTasks ? "" : <Form onSubmit={addTask}>
-            <Row>
-              <Col md="4">
-                <FormGroup>
-                  <Label for='taskname'>Task Name</Label>
-                  <Input
-                    type='text'
-                    id='taskname'
-                    value={taskName}
-                    onChange={(e) => setTaskName(e.target.value)}
-                    required
-                  />
-                </FormGroup>
-              </Col>
-              <Col md="4">
-                <FormGroup>
-                  <Label for='tasklogo'>Task File</Label>
-                  <CustomInput
-                    type='file'
-                    id='tasklogo'
-                    name='taskfile'
-                    onChange={(e) => setTaskFile(e.target.files[0])}
-                    required
-                  />
-                </FormGroup>
-              </Col>
-              <Col md="4" className="d-flex align-items-end">
-                <Button color="primary" type="submit" className="mb-1 float-right">Add a Task</Button>
-              </Col>
-            </Row>
-          </Form>
+          <Row>
+            <Col md="4">
+              <FormGroup>
+                <Label for='taskname'>Task Name</Label>
+                <Input
+                  type='text'
+                  id='taskname'
+                  value={taskName}
+                  onChange={(e) => setTaskName(e.target.value)}
+                  required
+                />
+              </FormGroup>
+            </Col>
+            <Col md="4">
+              <FormGroup>
+                <Label for='tasklogo'>Task File</Label>
+                <CustomInput
+                  type='file'
+                  id='tasklogo'
+                  name='taskfile'
+                  onChange={(e) => setTaskFile(e.target.files[0])}
+                  required
+                />
+              </FormGroup>
+            </Col>
+            <Col md="4" className="d-flex align-items-end">
+              <Button color="primary" type="submit" className="mb-1 float-right">Add a Task</Button>
+            </Col>
+          </Row>
+        </Form>
       }
       <DataTable
         noHeader
